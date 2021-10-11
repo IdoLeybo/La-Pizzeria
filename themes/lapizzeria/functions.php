5fd62e082a9a24554e9242b5fb488881802bbc29
IdoLeybo <Idol@moveo.co.il>
2021-10-11 14:03:07 +0300

    Added initial 'Header Menu' & 'Social Menu'

--- themes/lapizzeria/functions.php
+++ themes/lapizzeria/functions.php
@@ -11,3 +11,13 @@ function lapizzeria_styles() {
     wp_enqueue_style('style');
 }
 add_action('wp_enqueue_scripts', 'lapizzeria_styles');
+
+
+// Add Menus
+function lapizzeria_menus() {
+    register_nav_menus(array(
+        'header-menu' => __('Header Menu', 'lapizzeria'),
+        'social-menu' => __('Social Menu', 'lapizzeria')
+    ));
+}
+add_action('init', 'lapizzeria_menus');
