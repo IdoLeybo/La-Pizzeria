ae35c0b0cb50a732e633e1f72c7d1dae71b44c7c
IdoLeybo <Idol@moveo.co.il>
2021-10-14 09:12:35 +0300

    Sidebar & CREATE table in the database

--- themes/lapizzeria/functions.php
+++ themes/lapizzeria/functions.php
@@ -1,11 +1,20 @@
 <?php
 
+require get_template_directory() . '/inc/database.php';
+require get_template_directory() . '/inc/reservations.php';
+require get_template_directory() . '/inc/options.php';
+
+
 //Hook
 function lapizzeria_setup() {
     add_theme_support('post-thumbnails');
 
     add_image_size('boxes', 367, 291, true);
     add_image_size('specialties', 768, 515, true);
+    add_image_size('specialty-portrait', 435, 530, true);
+
+    update_option('thumbnail_size_w', 253);
+    update_option('thumbnail_size_h', 164);
 }
 add_action('after_setup_theme', 'lapizzeria_setup');
 
@@ -13,22 +22,36 @@ function lapizzeria_styles() {
     //adding stylesheet
     wp_register_style('googlefont', 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Raleway:wght@400;700;900&display=swap', array(), '2.0.0');
     wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
+    wp_register_style('fluidboxcss', get_template_directory_uri() . '/css/fluidbox.min.css', array(), '1.0.0');
     wp_register_style('fontawesome', get_template_directory_uri() . '/css/font-awesome.css', array(), '4.7.0');
     wp_register_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0');
 
     // Enqueue the styles
     wp_enqueue_style('normalize');
+    wp_enqueue_style('fluidboxcss');
     wp_enqueue_style('fontawesome');
     wp_enqueue_style('googlefont');
     wp_enqueue_style('style');
 
     /** Load JS Files */
 
+    wp_register_script('fluidboxjs', get_template_directory_uri() . '/js/jquery.fluidbox.min.js', array('jquery'), '1.0.0', true);
     //SlickNav Js
     wp_register_script('script', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0', true);
     //Enqueue the Script
     wp_enqueue_script('jquery');
+    wp_enqueue_script('fluidboxjs');
     wp_enqueue_script('script');
+
+    wp_localize_script(
+        'script',
+        'options',
+        array(
+            'latitude' => get_option('lapizzeria_gmap_latitude'),
+            'langitude' => get_option('lapizzeria_gmap_longitude'),
+            'zoom' => get_option('lapizzeria_gmap_zoom')
+        )
+    );
 }
 add_action('wp_enqueue_scripts', 'lapizzeria_styles');
 
@@ -80,3 +103,18 @@ function lapizzeria_specialties() {
     register_post_type( 'specialties', $args );
 }
 add_action( 'init', 'lapizzeria_specialties' );
+
+//Widgets Zone
+
+function lapizzeria_widgets() {
+    register_sidebar( array(
+       'name' =>  'Blog Sidebar',
+        'id' => 'blog_sidebar',
+        'before_widget' => '<div class="widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'
+    ));
+}
+add_action('widgets_init', 'lapizzeria_widgets');
+
